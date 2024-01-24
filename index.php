7fb3bcdf7fea3daf994f30c29f5dc3c5b73349d9
DejanDjosic <ddjosic@gmail.com>
2024-01-24 22:35:42 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php require('shared/navbar.php');
+require('shared/footer.php');?>
+
---
+++ shared/footer.php
@@ -0,0 +1,42 @@
+<footer class="text-white" >
+	<div class="container-fluid pt-5 pb-5">
+		<div class="row">
+			<div class="col-md-2">
+				<h1 class="font-weight-bold mt-5 pt-2">BRAND</h1>
+			</div>
+			<div class='col-md-3'>
+				<h4 class="font-weight-bold mb-3">GIFT CARDS</h4><hr/>
+				<p>PRODUCTIONS</p>
+				<p>FIND & STORE</p>
+				<p>SIGN UP MAIL</p>
+				<p>BOOK A MEMBER</p>
+				<p>BRAND SHOP</p>
+				<p>SEND US FEEDBACK</p>
+			</div>
+			<div class='col-md-3'>
+				<h4 class="font-weight-bold mb-3">GET HELP</h4><hr/>
+				<p>RETURNS</p>
+				<p>SHIPPING & DELIVERY</p>
+				<p>CONTACT US</p>
+				<p>BOOK A MEMBER</p>
+				<p>BRAND SHOP</p>
+				<p>SEND US FEEDBACK</p>
+			</div>
+			<div class='col-md-3'>
+				<h4 class="font-weight-bold mb-3">ABOUT BRAND</h4><hr/>
+				<p>NEWS</p>
+				<p>CAREERS</p>
+				<p>SIGN UP FOR MAIL</p>
+				<p>BOOK A MEMBER</p>
+				<p>BRAND SHOP</p>
+				<p>SEND US FEEDBACK</p>
+			</div>
+			<div class='col-md-1'>
+				<img src='logo.jpg' width="80px"/>
+				<h4 class='font-weight-bold' style="margin-top:200px;">BRAND</h4>
+			</div>
+		</div>
+	</div>
+</footer>
+</body>
+</html>
